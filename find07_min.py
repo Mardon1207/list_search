973e397ec6b45d1409adcef92dabd02da322de46
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-30 11:39:20 -0700

    7

--- find07_min.py
+++ find07_min.py
@@ -5,5 +5,14 @@ def find_min(data):
         data: list of numbers
     returns: minimum number in the list
     """
-
-    return 0
+    i=0
+    n=len(data)
+    m=data[0]
+    while i<n:
+        if data[i]<m:
+            m=data[i]
+        i+=1
+    return m
+data=[1,2,3,4,5,12,9,0]
+print(find_min(data))
+    
