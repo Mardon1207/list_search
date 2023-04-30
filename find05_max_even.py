8ce71d0cdf05251adf72df0986ff32ec16ebfca0
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-30 11:35:48 -0700

    5

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,4 +5,15 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    return 0
+    i=0
+    s=0
+    n=len(data)
+    m=data[0]
+    while i<n:
+        if data[i]>m and data[i]%2==0:
+            m=data[i]
+        i+=1
+    return m
+data=[1,2,13,4,5,12,9,0]
+print(find_max_even(data))
+    
