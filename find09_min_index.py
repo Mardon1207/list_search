25598619656e956e3719bab74bb96e80d2023e87
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-30 11:42:56 -0700

    9

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,17 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    i=0
+    s=0
+    n=len(data)
+    m=data[0]
+    while i<n:
+        if data[i]<m:
+            m=data[i]
+            s=i
+        i+=1
+    return s
+data=[1,2,13,4,5,12,9,0]
+print(find_min_index(data))
+    
 
